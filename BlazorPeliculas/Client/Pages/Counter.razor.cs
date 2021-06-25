5c59ddd35f64027755f14ae7369b2d1e55864ca5
Alejandro Gomez <41384332+alexgomz@users.noreply.github.com>
2021-06-25 13:03:08 -0400

    Using JS and C# functions. Isolating JS and CSS

--- BlazorPeliculas/Client/Pages/Counter.razor.cs
+++ BlazorPeliculas/Client/Pages/Counter.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,14 +12,35 @@ namespace BlazorPeliculas.Client.Pages
     {
         [Inject] ServicioSingleton singleton { get; set; }
         [Inject] ServicioTransient transient { get; set; }
+        [Inject] IJSRuntime JS { get; set; }
+
+        IJSObjectReference modulo;
 
         private int currentCount = 0;
+        static int currentCountStatic = 0;
 
-        private void IncrementCount()
+        [JSInvokable]
+        public async Task IncrementCount()
         {
+            modulo = await JS.InvokeAsync<IJSObjectReference>("import", "./JS/Counter.js");
+            await modulo.InvokeVoidAsync("mostrarAlerta", "hola mundo");
+
             currentCount++;
             singleton.Valor = currentCount;
             transient.Valor = currentCount;
+            currentCountStatic++;
+            await JS.InvokeVoidAsync("pruebaPuntoNetStatic");
+        }
+
+        protected async Task IncrementCountJavascript()
+        {
+            await JS.InvokeVoidAsync("pruebaPuntoNetInstancia", DotNetObjectReference.Create(this));
+        }
+
+        [JSInvokable]
+        public static Task<int> ObtenerCurrentCount()
+        {
+            return Task.FromResult(currentCountStatic);
         }
     }
 }
---
+++ BlazorPeliculas/Client/wwwroot/JS/Counter.js
@@ -0,0 +1,6 @@
+﻿
+//Use as a module, to only load when used instead of when loading page
+//as most JS does
+export function mostrarAlerta(mensaje) {
+    return alert(mensaje);
+}
